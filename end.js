38b207b91766a27ad74b2421fd5273b0143f6451
saini-manisha <manishasaini46160@gmail.com>
2024-12-04 14:30:13 +0530

    save high score in localstorge

--- end.js
+++ end.js
@@ -1,9 +1,8 @@
 const username=document.getElementById("username");
 const saveScoreBtn=document.getElementById("saveScoreBtn");
 const finalScore=document.getElementById("finalScore");
-
-
 const mostRecentScore=localStorage.getItem("mostRecentScore");
+const highScore=JSON.parse(localStorage.getItem("highScore")) ||[];
 finalScore.innerText=mostRecentScore
 username.addEventListener("keyup",()=>{
     console.log(username.value);
@@ -12,6 +11,14 @@ username.addEventListener("keyup",()=>{
 })
 const saveHighScore=(e)=>{
     e.preventDefault();
-    localStorage.setItem(username.value,localStorage.getItem("mostRecentScore"));
     
+    const score={
+        name:username.value,
+        score:mostRecentScore
+    }
+    highScore.push(score);
+    highScore.sort((a,b)=>b.score-a.score);
+    highScore.splice(5);
+    localStorage.setItem("highScore",JSON.stringify(highScore));
+    window.location.assign("index.html");
 }
